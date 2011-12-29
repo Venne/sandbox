975f0f18f16545279350246f7663977ca055191d
Josef Kříž <pepakriz@gmail.com>
2011-12-29 10:21:11 +0100

    ported to lattest Venne

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -7,6 +7,7 @@
 $parameters['rootDir'] = realpath(__DIR__ . '/..');
 $parameters['tempDir'] = $parameters['rootDir'] . '/temp';
 $parameters['libsDir'] = $parameters['rootDir'] . '/libs';
+$parameters['logDir'] = $parameters['rootDir'] . '/log';
 $parameters['venneDir'] = $parameters['libsDir'] . '/Venne';
 $parameters['appDir'] = $parameters['rootDir'] . '/app';
 $parameters['wwwDir'] = $parameters['rootDir'] . '/www';
