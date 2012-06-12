44e22f1d1b13ef30d4e466e22d65b422f933aa91
Josef Kříž <pepakriz@gmail.com>
2012-06-12 09:36:14 +0200

    new structure

--- www/index.php
+++ www/index.php
@@ -3,7 +3,14 @@
 // uncomment this line if you must temporarily take down your site for maintenance
 // require '.maintenance.php';
 // load Venne:CMS
-require_once __DIR__ . '/../vendor/venne/cms/Venne/loader.php';
+require_once __DIR__ . '/../vendor/venne/venne/Venne/loader.php';
+
+// create default configuration
+foreach (array("settings.php", "config.neon") as $config) {
+	if (!file_exists(__DIR__ . '/../app/config/' . $config)) {
+		copy(__DIR__ . '/../app/config/' . $config . '.orig', __DIR__ . '/../app/config/' . $config);
+	}
+}
 
 // load settings
 $settings = require_once __DIR__ . '/../app/config/settings.php';
