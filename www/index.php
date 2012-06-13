b97a1036881e382508395ba26f93f105ed19bf5e
Josef Kříž <pepakriz@gmail.com>
2012-06-13 17:28:51 +0200

    Updated default configs

--- www/index.php
+++ www/index.php
@@ -6,14 +6,14 @@
 require_once __DIR__ . '/../vendor/venne/venne/Venne/loader.php';
 
 // create default configuration
-foreach (array("settings.php", "config.neon") as $config) {
+foreach (array("settings.php", "config.neon", 'modules.neon') as $config) {
 	if (!file_exists(__DIR__ . '/../app/config/' . $config)) {
 		copy(__DIR__ . '/../app/config/' . $config . '.orig', __DIR__ . '/../app/config/' . $config);
 	}
 }
 
 // load settings
-$settings = require_once __DIR__ . '/../app/config/settings.php';
+$settings = require __DIR__ . '/../app/config/settings.php';
 
 // create and run application
 $configurator = new \Venne\Config\Configurator($settings);
