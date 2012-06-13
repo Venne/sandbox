10cbb0a1811d86ffca7fa7c9e305f83180a96361
Josef Kříž <pepakriz@gmail.com>
2012-06-13 20:06:35 +0200

    Updated structure

--- www/index.php
+++ www/index.php
@@ -3,7 +3,9 @@
 // uncomment this line if you must temporarily take down your site for maintenance
 // require '.maintenance.php';
 // load Venne:CMS
-require_once __DIR__ . '/../vendor/venne/venne/Venne/loader.php';
+require_once __DIR__ . '/../vendor/venne/framework/Venne/loader.php';
+
+\Nette\Diagnostics\Debugger::enable(\Nette\Diagnostics\Debugger::DEVELOPMENT);
 
 // create default configuration
 foreach (array("settings.php", "config.neon", 'modules.neon') as $config) {
@@ -11,6 +13,8 @@ foreach (array("settings.php", "config.neon", 'modules.neon') as $config) {
 		copy(__DIR__ . '/../app/config/' . $config . '.orig', __DIR__ . '/../app/config/' . $config);
 	}
 }
+
+\Nette\Diagnostics\Debugger::enable(\Nette\Diagnostics\Debugger::DEVELOPMENT);
 
 // load settings
 $settings = require __DIR__ . '/../app/config/settings.php';
