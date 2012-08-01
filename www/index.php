0c3fb290a276d0e86931fe97e67902238732e1fa
Josef Kříž <pepakriz@gmail.com>
2012-08-01 16:00:23 +0200

    Ported to new Configurator

--- www/index.php
+++ www/index.php
@@ -5,20 +5,8 @@
 // load Venne:CMS
 require_once __DIR__ . '/../vendor/autoload.php';
 
-\Nette\Diagnostics\Debugger::enable(\Nette\Diagnostics\Debugger::DEVELOPMENT);
-
-// create default configuration
-foreach (array("settings.php", "config.neon", 'modules.neon') as $config) {
-	if (!file_exists(__DIR__ . '/../app/config/' . $config)) {
-		copy(__DIR__ . '/../app/config/' . $config . '.orig', __DIR__ . '/../app/config/' . $config);
-	}
-}
-
-// load settings
-$settings = require __DIR__ . '/../app/config/settings.php';
-
 // create and run application
-$configurator = new \Venne\Config\Configurator($settings);
+$configurator = new \Venne\Config\Configurator(dirname(__DIR__));
 $configurator->enableDebugger();
 $configurator->enableLoader();
 $configurator->getContainer()->application->run();
