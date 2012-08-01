b4cda794092f1903b728ea4127c15f4393b272c2
Josef Kříž <pepakriz@gmail.com>
2012-08-01 15:58:37 +0200

    Added sandbox.php

---
+++ sandbox.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+	'appDir'        => __DIR__ . '/app',
+	'configDir'     => __DIR__ . '/app/config',
+	'logDir'        => __DIR__ . '/app/log',
+	'dataDir'       => __DIR__ . '/app/data',
+	'tempDir'       => __DIR__ . '/app/temp',
+	'libsDir'       => __DIR__ . '/vendor',
+	'wwwDir'        => __DIR__ . '/www',
+	'wwwCacheDir'   => __DIR__ . '/www/cache',
+	'resourcesDir'  => __DIR__ . '/www/resources',
+);
