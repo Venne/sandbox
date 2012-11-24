d48721bf1d402c7f238778983f0c09300c2b47bd
Josef Kříž <pepakriz@gmail.com>
2012-11-24 18:38:49 +0100

    Structure for new modules.

--- www/index.php
+++ www/index.php
@@ -3,10 +3,12 @@
 // uncomment this line if you must temporarily take down your site for maintenance
 // require '.maintenance.php';
 // load Venne:CMS
-require_once __DIR__ . '/../vendor/autoload.php';
+
+/** @var $loader Composer\Autoload\ClassLoader */
+$loader = require_once __DIR__ . '/../vendor/autoload.php';
 
 // create and run application
-$configurator = new \Venne\Config\Configurator(dirname(__DIR__));
+$configurator = new \Venne\Config\Configurator(dirname(__DIR__), $loader);
 $configurator->enableDebugger();
 $configurator->enableLoader();
 $configurator->getContainer()->application->run();
